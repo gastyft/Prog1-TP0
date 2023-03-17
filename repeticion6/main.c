32574b018cfb4d2b9f21c6a2193fee4515549c95
gastyftacla <99477542+gastyft@users.noreply.github.com>
2023-03-17 19:01:33 -0300

    Update main.c

--- repeticion6/main.c
+++ repeticion6/main.c
@@ -1,11 +1,23 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*Calcular e imprimir la tabla de multiplicar de un número cualquiera.
+/*Calcular e imprimir la tabla de multiplicar de un nÃºmero cualquiera.
 Imprimir el multiplicando, el multiplicador y el producto.*/
 
 int main()
 {
+      int num,i=0,t=1;
+    printf("Ingrese el numero que desea imprimir\n");
+    scanf("%d",&num);
+
+
+    do
+    {
+    t=i*num;
+        printf("%d x %d = %d \n\n",num,i,t);
+    i++;
+    }
+    while(i<11);
 
     return 0;
 }
